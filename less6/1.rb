eeec86a91e39ade25e9db1250f478e96e6ef5879
Kuat81 <51964429+Kuat81@users.noreply.github.com>
2019-07-09 18:38:29 +0600

    Add files via upload

--- less6/1.rb
+++ less6/1.rb
@@ -1,39 +1,43 @@
 def ask(string)
+
   days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 pust 'Incorrect Request' unless string.class == String
 
- puts Time.now.strftime("%d/%m") if string == 'time'
+case string
+  when "time"
+    puts Time.now.strftime("%d/%m") if string == 'time'
+  when 'date'
+    puts Time.now.strftime("%d/%m/%y") if string == 'date'
+  when 'day'
+    puts Time.now.strftime("%A, %d/%m/%y") if string == 'day'
+  when 'remaining days'
+    if string == 'remaining days'
+      day = Time.now.strftime ("%d")
+       mon = Time.now.strftime ("%m")
+        yer = Time.now.strftime ("%Y")
 
- puts Time.now.strftime("%d/%m/%y") if string == 'date'
+      d = day.to_i
+      m = mon.to_i
+      y = yer.to_i
+       days[1] = 29 if y % 400 == 0 || ( y % 4 ==0 && y % 100 != 0)
+      puts days.sum - (days[0..(m-1)].sum + d)
+    end
+when 'remaining weeks'
+    if string == 'remaining weeks'
+      day = Time.now.strftime ("%d")
+      mon = Time.now.strftime ("%m")
+      yer = Time.now.strftime ("%Y")
 
- puts Time.now.strftime("%A, %d/%m/%y") if string == 'day'
+      d = day.to_i
+      m = mon.to_i
+      y = yer.to_i
+      days[1] = 29 if y % 400 == 0 || ( y % 4 ==0 && y % 100 != 0)
+      puts (days.sum - (days[0..(m-1)].sum + d)) / 7
 
-if string == 'remaining days'
-  day = Time.now.strftime ("%d")
-  mon = Time.now.strftime ("%m")
-  yer = Time.now.strftime ("%Y")
+    end
 
-  d = day.to_i
-  m = mon.to_i
-  y = yer.to_i
-  days[1] = 29 if y % 400 == 0 || ( y % 4 ==0 && y % 100 != 0)
-  puts days.sum - (days[0..(m-1)].sum + d)
+  end
 end
 
-if string == 'remaining weeks'
-  day = Time.now.strftime ("%d")
-  mon = Time.now.strftime ("%m")
-  yer = Time.now.strftime ("%Y")
-
-  d = day.to_i
-  m = mon.to_i
-  y = yer.to_i
-  days[1] = 29 if y % 400 == 0 || ( y % 4 ==0 && y % 100 != 0)
-  puts (days.sum - (days[0..(m-1)].sum + d)) / 7
-
-end
-
-end
-
-ask('remaining weeks')
+ask('day')
